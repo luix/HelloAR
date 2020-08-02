4a09a41036b728f1cbc67c5ca9affa5f2806bce1
LuiX <luis.a.vivero@gmail.com>
2020-08-01 20:37:04 -0700

    Update MainActivity to init Filament utils

--- app/src/main/java/mx/luix/arjesus/MainActivity.kt
+++ app/src/main/java/mx/luix/arjesus/MainActivity.kt
@@ -2,8 +2,16 @@ package mx.luix.arjesus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.filament.utils.Utils
+
+//import com.google.ar.sceneform
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        init { Utils.init() }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
