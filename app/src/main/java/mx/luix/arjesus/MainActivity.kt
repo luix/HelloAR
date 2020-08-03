2f5f5803317ef24279a909a05864f7e34e7c12b5
LuiX <luis.a.vivero@gmail.com>
2020-08-03 11:25:35 -0700

    Allow Filament to render during the frame callback.

--- app/src/main/java/mx/luix/arjesus/MainActivity.kt
+++ app/src/main/java/mx/luix/arjesus/MainActivity.kt
@@ -2,11 +2,25 @@ package mx.luix.arjesus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Choreographer
+import android.view.SurfaceView
+import com.google.android.filament.utils.ModelViewer
 import com.google.android.filament.utils.Utils
 
 //import com.google.ar.sceneform
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var choreographer: Choreographer
+    private lateinit var modelViewer: ModelViewer
+
+    private val frameCallback = object : Choreographer.FrameCallback {
+        override fun doFrame(currentTime: Long) {
+            choreographer.postFrameCallback(this)
+            modelViewer.render(currentTime)
+        }
+    }
 
     companion object {
         init { Utils.init() }
@@ -14,6 +28,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        surfaceView = SurfaceView(this).apply { setContentView(this) }
+        choreographer = Choreographer.getInstance()
+        modelViewer = ModelViewer(surfaceView)
+        surfaceView.setOnTouchListener(modelViewer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        choreographer.postFrameCallback(frameCallback)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        choreographer.removeFrameCallback(frameCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        choreographer.postFrameCallback(frameCallback)
     }
 }
