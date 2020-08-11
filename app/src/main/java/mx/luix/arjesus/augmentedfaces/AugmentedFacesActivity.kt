bff4c50fc543cbea4ac21f11bab25ad7f0ccf88d
LuiX <luis.a.vivero@gmail.com>
2020-08-10 20:20:29 -0700

    Convert CameraPermissionHelper.java to Kotlin source code.

---
+++ app/src/main/java/mx/luix/arjesus/augmentedfaces/AugmentedFacesActivity.kt
@@ -0,0 +1,353 @@
+/*
+ * Copyright 2020 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package mx.luix.arjesus.augmentedfaces
+
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.*
+import com.google.ar.core.ArCoreApk.InstallStatus
+import com.google.ar.core.AugmentedFace.RegionType
+import com.google.ar.core.Config.AugmentedFaceMode
+import com.google.ar.core.Session.Feature
+import com.google.ar.core.exceptions.*
+import mx.luix.arjesus.R
+import mx.luix.arjesus.common.helpers.CameraPermissionHelper.hasCameraPermission
+import mx.luix.arjesus.common.helpers.CameraPermissionHelper.launchPermissionSettings
+import mx.luix.arjesus.common.helpers.CameraPermissionHelper.requestCameraPermission
+import mx.luix.arjesus.common.helpers.CameraPermissionHelper.shouldShowRequestPermissionRationale
+import mx.luix.arjesus.common.helpers.DisplayRotationHelper
+import mx.luix.arjesus.common.helpers.FullScreenHelper.setFullScreenOnWindowFocusChanged
+import mx.luix.arjesus.common.helpers.SnackbarHelper
+import mx.luix.arjesus.common.helpers.TapHelper
+import mx.luix.arjesus.common.helpers.TrackingStateHelper
+import mx.luix.arjesus.common.rendering.BackgroundRenderer
+import mx.luix.arjesus.common.rendering.ObjectRenderer
+import java.io.IOException
+import java.util.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+//import android.support.v7.app.AppCompatActivity;
+/**
+ * This is a simple example that shows how to create an augmented reality (AR) application using the
+ * ARCore API. The application will display any detected planes and will allow the user to tap on a
+ * plane to place a 3d model of the Android robot.
+ */
+class AugmentedFacesActivity : AppCompatActivity(), GLSurfaceView.Renderer {
+    // Rendering. The Renderers are created here, and initialized when the GL surface is created.
+    private var surfaceView: GLSurfaceView? = null
+    private var installRequested = false
+    private var session: Session? = null
+    private val messageSnackbarHelper = SnackbarHelper()
+    private var displayRotationHelper: DisplayRotationHelper? = null
+    private val trackingStateHelper = TrackingStateHelper(this)
+    private var tapHelper: TapHelper? = null
+    private val backgroundRenderer = BackgroundRenderer()
+    private val augmentedFaceRenderer = AugmentedFaceRenderer()
+    private val noseObject = ObjectRenderer()
+    private val rightEarObject = ObjectRenderer()
+    private val leftEarObject = ObjectRenderer()
+
+    // Temporary matrix allocated here to reduce number of allocations for each frame.
+    private val noseMatrix = FloatArray(16)
+    private val rightEarMatrix = FloatArray(16)
+    private val leftEarMatrix = FloatArray(16)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_augmented_faces)
+        surfaceView = findViewById(R.id.surfaceview)
+        displayRotationHelper = DisplayRotationHelper( /*context=*/this)
+
+        // Set up tap listener.
+        tapHelper = TapHelper( /*context=*/this)
+        surfaceView.setOnTouchListener(tapHelper)
+
+        // Set up renderer.
+        surfaceView.setPreserveEGLContextOnPause(true)
+        surfaceView.setEGLContextClientVersion(2)
+        surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0) // Alpha used for plane blending.
+        surfaceView.setRenderer(this)
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY)
+        surfaceView.setWillNotDraw(false)
+        installRequested = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (session == null) {
+            var exception: Exception? = null
+            var message: String? = null
+            try {
+                when (ArCoreApk.getInstance().requestInstall(this, !installRequested)) {
+                    InstallStatus.INSTALL_REQUESTED -> {
+                        installRequested = true
+                        return
+                    }
+                    InstallStatus.INSTALLED -> {
+                    }
+                }
+
+                // ARCore requires camera permissions to operate. If we did not yet obtain runtime
+                // permission on Android M and above, now is a good time to ask the user for it.
+                if (!hasCameraPermission(this)) {
+                    requestCameraPermission(this)
+                    return
+                }
+
+                // Configure session to use front facing camera.
+                val featureSet =
+                    EnumSet.of(Feature.FRONT_CAMERA)
+                // Create the session.
+                session = Session( /* context= */this, featureSet)
+                configureSession()
+            } catch (e: UnavailableArcoreNotInstalledException) {
+                message = "Please install ARCore"
+                exception = e
+            } catch (e: UnavailableUserDeclinedInstallationException) {
+                message = "Please install ARCore"
+                exception = e
+            } catch (e: UnavailableApkTooOldException) {
+                message = "Please update ARCore"
+                exception = e
+            } catch (e: UnavailableSdkTooOldException) {
+                message = "Please update this app"
+                exception = e
+            } catch (e: UnavailableDeviceNotCompatibleException) {
+                message = "This device does not support AR"
+                exception = e
+            } catch (e: Exception) {
+                message = "Failed to create AR session"
+                exception = e
+            }
+            if (message != null) {
+                messageSnackbarHelper.showError(this, message)
+                Log.e(
+                    TAG,
+                    "Exception creating session",
+                    exception
+                )
+                return
+            }
+        }
+
+        // Note that order matters - see the note in onPause(), the reverse applies here.
+        try {
+            session!!.resume()
+        } catch (e: CameraNotAvailableException) {
+            messageSnackbarHelper.showError(this, "Camera not available. Try restarting the app.")
+            session = null
+            return
+        }
+        surfaceView!!.onResume()
+        displayRotationHelper!!.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (session != null) {
+            // Note that the order matters - GLSurfaceView is paused first so that it does not try
+            // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
+            // still call session.update() and get a SessionPausedException.
+            displayRotationHelper!!.onPause()
+            surfaceView!!.onPause()
+            session!!.pause()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, results)
+        if (!hasCameraPermission(this)) {
+            Toast.makeText(
+                this,
+                "Camera permission is needed to run this application",
+                Toast.LENGTH_LONG
+            )
+                .show()
+            if (!shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                launchPermissionSettings(this)
+            }
+            finish()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        setFullScreenOnWindowFocusChanged(this, hasFocus)
+    }
+
+    override fun onSurfaceCreated(
+        gl: GL10,
+        config: EGLConfig
+    ) {
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
+
+        // Prepare the rendering objects. This involves reading shaders, so may throw an IOException.
+        try {
+            // Create the texture and pass it to ARCore session to be filled during update().
+            backgroundRenderer.createOnGlThread( /*context=*/this)
+            augmentedFaceRenderer.createOnGlThread(this)
+            augmentedFaceRenderer.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f)
+            noseObject.createOnGlThread( /*context=*/
+                this,
+                "models/NOSE.obj",
+                "models/nose_fur.png"
+            )
+            noseObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f)
+            noseObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending)
+            rightEarObject.createOnGlThread(
+                this, "models/FOREHEAD_RIGHT.obj", "models/ear_fur.png"
+            )
+            rightEarObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f)
+            rightEarObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending)
+            leftEarObject.createOnGlThread(
+                this, "models/FOREHEAD_LEFT.obj", "models/ear_fur.png"
+            )
+            leftEarObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f)
+            leftEarObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending)
+        } catch (e: IOException) {
+            Log.e(
+                TAG,
+                "Failed to read an asset file",
+                e
+            )
+        }
+    }
+
+    override fun onSurfaceChanged(gl: GL10, width: Int, height: Int) {
+        displayRotationHelper!!.onSurfaceChanged(width, height)
+        GLES20.glViewport(0, 0, width, height)
+    }
+
+    override fun onDrawFrame(gl: GL10) {
+        // Clear screen to notify driver it should not load any pixels from previous frame.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+        if (session == null) {
+            return
+        }
+        // Notify ARCore session that the view size changed so that the perspective matrix and
+        // the video background can be properly adjusted.
+        displayRotationHelper!!.updateSessionIfNeeded(session!!)
+        try {
+            session!!.setCameraTextureName(backgroundRenderer.textureId)
+
+            // Obtain the current frame from ARSession. When the configuration is set to
+            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+            // camera framerate.
+            val frame = session!!.update()
+            val camera = frame.camera
+
+            // Get projection matrix.
+            val projectionMatrix = FloatArray(16)
+            camera.getProjectionMatrix(projectionMatrix, 0, 0.1f, 100.0f)
+
+            // Get camera matrix and draw.
+            val viewMatrix = FloatArray(16)
+            camera.getViewMatrix(viewMatrix, 0)
+
+            // Compute lighting from average intensity of the image.
+            // The first three components are color scaling factors.
+            // The last one is the average pixel intensity in gamma space.
+            val colorCorrectionRgba = FloatArray(4)
+            frame.lightEstimate.getColorCorrection(colorCorrectionRgba, 0)
+
+            // If frame is ready, render camera preview image to the GL surface.
+            backgroundRenderer.draw(frame)
+
+            // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
+            trackingStateHelper.updateKeepScreenOnFlag(camera.trackingState)
+            val faces =
+                session!!.getAllTrackables(
+                    AugmentedFace::class.java
+                )
+            for (face in faces) {
+                if (face.trackingState != TrackingState.TRACKING) {
+                    break
+                }
+                val scaleFactor = 1.0f
+
+                // Face objects use transparency so they must be rendered back to front without depth write.
+                GLES20.glDepthMask(false)
+
+                // Each face's region poses, mesh vertices, and mesh normals are updated every frame.
+
+                // 1. Render the face mesh first, behind any 3D objects attached to the face regions.
+                val modelMatrix = FloatArray(16)
+                face.centerPose.toMatrix(modelMatrix, 0)
+                augmentedFaceRenderer.draw(
+                    projectionMatrix, viewMatrix, modelMatrix, colorCorrectionRgba, face
+                )
+
+                // 2. Next, render the 3D objects attached to the forehead.
+                face.getRegionPose(RegionType.FOREHEAD_RIGHT).toMatrix(rightEarMatrix, 0)
+                rightEarObject.updateModelMatrix(rightEarMatrix, scaleFactor)
+                rightEarObject.draw(
+                    viewMatrix,
+                    projectionMatrix,
+                    colorCorrectionRgba,
+                    DEFAULT_COLOR
+                )
+                face.getRegionPose(RegionType.FOREHEAD_LEFT).toMatrix(leftEarMatrix, 0)
+                leftEarObject.updateModelMatrix(leftEarMatrix, scaleFactor)
+                leftEarObject.draw(
+                    viewMatrix,
+                    projectionMatrix,
+                    colorCorrectionRgba,
+                    DEFAULT_COLOR
+                )
+
+                // 3. Render the nose last so that it is not occluded by face mesh or by 3D objects attached
+                // to the forehead regions.
+                face.getRegionPose(RegionType.NOSE_TIP).toMatrix(noseMatrix, 0)
+                noseObject.updateModelMatrix(noseMatrix, scaleFactor)
+                noseObject.draw(
+                    viewMatrix,
+                    projectionMatrix,
+                    colorCorrectionRgba,
+                    DEFAULT_COLOR
+                )
+            }
+        } catch (t: Throwable) {
+            // Avoid crashing the application due to unhandled exceptions.
+            Log.e(
+                TAG,
+                "Exception on the OpenGL thread",
+                t
+            )
+        } finally {
+            GLES20.glDepthMask(true)
+        }
+    }
+
+    private fun configureSession() {
+        val config = Config(session)
+        config.augmentedFaceMode = AugmentedFaceMode.MESH3D
+        session!!.configure(config)
+    }
+
+    companion object {
+        private val TAG = AugmentedFacesActivity::class.java.simpleName
+        private val DEFAULT_COLOR = floatArrayOf(0f, 0f, 0f, 0f)
+    }
+}
